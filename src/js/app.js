c58d902e7d6d6edbfd83206a24cbf129b27cb4b7
Mike Borsare <mborsare@gmail.com>
2018-04-25 12:01:40 -0400

    Roughed in share button states and began compare page

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
 import setPathCookie from './utils/setPathCookie.js'
 import removeMobileHover from './utils/removeMobileHover.js'
+import sendButtonMessage from "./sendButtonMessage.js"
 
 removeMobileHover()
 setPathCookie()
+sendButtonMessage()
 
 // Add class to html if JS is loaded
 document.querySelector('html').classList.add('js-is-loaded')
--- src/js/critical.js
+++
@@ -1,6 +0,0 @@
-import loadFont from './utils/loadFont'
-
-loadFont([
-	// Load fonts here
-	// { family: '[FAMILY]', suffix: '[BOLD, REGULAR, ITALIC]'}
-])
