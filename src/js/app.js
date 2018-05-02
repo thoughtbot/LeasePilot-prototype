f035bc551471334a144ce97f3b105598b2dff17a
Paul Smith <paulcsmith0218@gmail.com>
2018-05-02 15:27:25 -0400

    Stuff

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,23 @@
 import setPathCookie from "./utils/setPathCookie.js";
 import removeMobileHover from "./utils/removeMobileHover.js";
-import Commentable from "./commentable.js";
-import ShareForm from "./share-form.js";
-import CompareHeader from "./compare-header";
-import VueApp from "./vue-app.js";
-import Vue from "vue";
+// import Commentable from "./commentable.js";
+// import ShareForm from "./share-form.js";
+// import CompareHeader from "./compare-header";
+// import VueApp from "./vue-app.js";
+// import Vue from "vue";
 
-Vue.config.ignoredElements = [
-  "lease-var",
-  "building-variable",
-  "lease-attachment",
-  "crb",
-  "lease-start",
-  "lease-end"
-];
+// Vue.config.ignoredElements = [
+//   "lease-var",
+//   "building-variable",
+//   "lease-attachment",
+//   "crb",
+//   "lease-start",
+//   "lease-end"
+// ];
 
 removeMobileHover();
 setPathCookie();
-new VueApp();
+// new VueApp();
 
 // Add class to html if JS is loaded
 document.querySelector("html").classList.add("js-is-loaded");
